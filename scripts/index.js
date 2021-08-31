80d0bd228f0a8cf1201e6f438dde806c9fd3f5f5
Yana Zamyatina <zyanets@yandex.ru>
2021-08-31 22:35:50 +0900

    fix: устранить замечания

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,39 @@
+/* ---------------------- Переменные ----------------------*/
+
 let profileEdit = document.querySelector('.profile__button-edit');
 let popup = document.querySelector('.popup');
 let usernameInput = document.querySelector('.popup__input_type_username');
 let username = document.querySelector('.profile__username');
 let occupationInput = document.querySelector('.popup__input_type_occupation');
 let occupation = document.querySelector('.profile__occupation');
-let save = document.querySelector('.popup__button-save');
 let formClose = document.querySelector('.popup__button-close');
 let formElement = document.querySelector('.popup__form');
 
-function togglePopup () {
+/* ---------------------- Функция: открыть форму ----------------------*/
+function openPopup () {
   if (!popup.classList.contains('popup_opened')) {
     usernameInput.value = username.textContent;
     occupationInput.value = occupation.textContent;
   }
-  popup.classList.toggle('popup_opened')
+  popup.classList.add('popup_opened')
 }
 
+/* ---------------------- Функция: закрыть форму ----------------------*/
+function closePopup () {
+  if (popup.classList.contains('popup_opened')) {
+  }
+    popup.classList.remove('popup_opened')
+}
+
+/* ---------------------- Функция: сохранить введенные данные ----------------------*/
 function formSubmitHandler (evt) {
     evt.preventDefault();
     username.textContent = usernameInput.value;
     occupation.textContent = occupationInput.value;
-    togglePopup();
+    closePopup();
 }
 
-profileEdit.addEventListener('click', togglePopup);
-formClose.addEventListener('click', togglePopup);
+/* ---------------------- Отследить действия ----------------------*/
+profileEdit.addEventListener('click', openPopup);
+formClose.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
